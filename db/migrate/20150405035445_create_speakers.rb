53cdaa8f9a0c2db00131de8762c0b56948aef66f
beth laing <laingbeth@gmail.com>
2015-04-04 23:57:24 -0400

    first commit

---
+++ db/migrate/20150405035445_create_speakers.rb
@@ -0,0 +1,13 @@
+class CreateSpeakers < ActiveRecord::Migration
+  def change
+    create_table :speakers do |t|
+      t.string :name
+      t.string :contact
+      t.text :description
+      t.string :headline
+      t.string :topics
+
+      t.timestamps null: false
+    end
+  end
+end
